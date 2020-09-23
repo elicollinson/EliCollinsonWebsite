208a13f3871f00f61ff40badf95995bb99a4e0bd
Eli Collinson <elicollinson1@gmail.com>
2020-09-23 17:01:31 -0400

    Added basic homepage and router functionality

--- eli-collinson-website/src/App.vue
+++ eli-collinson-website/src/App.vue
@@ -1,28 +1,49 @@
 <template>
+<!--
+  Colors:
+  Alabaster: #F1F0EA
+-->
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">My Story</a>
+      <a class="nav-item nav-link" href="#"></a>
+      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </div>
+  </div>
+</nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
 <style>
+* {
+  background-color: #F1F0EA;
+}
+
+a {
+  color: #D0EFB1;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #08415C;
   margin-top: 60px;
 }
 </style>
---
+++ eli-collinson-website/src/views/Home.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="mainPageDiv">
+    <h1>Eli's Awesome Site</h1>
+    <div id="intro-and-portrait-container">
+      <div id="camping-portrait-container">
+        <img alt="standing in the road" src="../assets/Camping-9.jpg" id="camping-portrait">
+      </div>
+      <div id="site-text">
+      <div id="site-introduction">
+        <p class="left-justified"> I am a recent .NET Core Full Stack Development Bootcamp Grad looking for work! If you like this site shoot me an email at elicollinson1@gmail.com. </p>
+      </div>
+      <div id="bootcamp-info">
+        <p>Here is the bootcamp I went through:</p>
+        <a href="https://www.techelevator.com/columbus">Tech Elevator</a>
+      </div>
+      <div id="site-creation-info">
+        <p>This site was built using vue.js with javascript, HTML5, CSS, and bootstrap.</p>
+      </div>
+      </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#intro-and-portrait-container {
+  /*display: grid;
+  justify-content: center;
+  grid-template-areas: "site-intro camping-portrait-container"
+                       "bootcamp-info camping-portrait-container"
+                       "site-creation-info camping-portrait-container";
+                       */
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-evenly;
+}
+
+#site-text{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+#camping-portrait-container {
+  /*grid-area: camping-portrait-container;*/
+  max-width: 30%;
+  background-color: #77867F;
+  padding: 1rem;
+  margin-right: 3rem;
+}
+
+#camping-portrait {
+  max-width: 100%;
+}
+
+#site-introduction {
+  /*grid-area: site-intro;*/
+  text-align: left;
+}
+
+#bootcamp-info{
+ /* grid-area: bootcamp-info;*/
+}
+
+#site-creation-info{
+  /* grid-area: site-creation-info;*/
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #CD8987;
+}
+
+.left-justified {
+  padding-left: 3rem;
+  padding-right: 3rem;
+}
+* {
+  color: #08415C;
+}
+</style>
