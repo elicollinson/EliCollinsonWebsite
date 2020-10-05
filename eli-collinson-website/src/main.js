38f93d823aa267899c55841b3e2decdab04c070c
Eli Collinson <elicollinson1@gmail.com>
2020-10-04 22:04:09 -0400

    Added vue-analytics package

--- eli-collinson-website/src/main.js
+++ eli-collinson-website/src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import router from '../router/index'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueAnalytics, {
+  id: 'UA-12345678-1',
+  router
+})
 
 new Vue({
   router,
