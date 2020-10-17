227b392d39663e2d7acdeb6d9512027373bdcba7
Eli Collinson <elicollinson1@gmail.com>
2020-10-17 13:45:40 -0400

    fixed build syntax errors

--- eli-collinson-website/src/main.js
+++ eli-collinson-website/src/main.js
@@ -15,5 +15,5 @@ Vue.use(VueAnalytics, {
 new Vue({
   router,
   render: h => h(App),
-  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
+  mounted: () => document.dispatchEvent(new Event('x-app-rendered'))
 }).$mount('#app')
