04c98dfa01b167496951adc74b161c91714ce529
Eli Collinson <elicollinson1@gmail.com>
2020-10-18 22:41:19 -0400

    Trailing spaces fix

--- eli-collinson-website/src/views/ContactMe.vue
+++ eli-collinson-website/src/views/ContactMe.vue
@@ -16,11 +16,11 @@
     -->
     <form name="contact" method="POST" data-netlify="true">
       <p>
-        <label>Your Name: <input type="text" name="name" /></label>   
+        <label>Your Name: <input type="text" name="name" /></label>
       </p>
       <p>
         <label>Your Email: <input type="email" name="email" /></label>
-      </p>      
+      </p>
     <p>
       <label>Message: <textarea name="message"></textarea></label>
     </p>
