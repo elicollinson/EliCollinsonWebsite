dfdef749f9afe198fe1189bf8ba3c72a680c9731
Eli Collinson <elicollinson1@gmail.com>
2020-10-25 17:12:56 -0400

    Added beginnings of AWS hosting and changed form name case to match for netlify form

--- eli-collinson-website/src/views/ContactMe.vue
+++ eli-collinson-website/src/views/ContactMe.vue
@@ -4,13 +4,13 @@
     <p>Right now this form is sending messages, but not capturing the data, so if you need to get in touch, email me at elicollinson1@gmail.com</p>
     <form class="container-fluid" id="contact-form" name="contact" method="POST" data-netlify=true @submit.prevent="handleSubmit">
         <label for="FullName">Please Enter Full Name</label>
-        <input class="form-control my-size" type="text" name="FullName" v-model="form.fullName" required/>
+        <input class="form-control my-size" type="text" name="fullName" v-model="form.fullName" required/>
         <label for="Email">Email</label>
-        <input class="form-control my-size" type="email" name="Email" v-model="form.email" required/>
+        <input class="form-control my-size" type="email" name="email" v-model="form.email" required/>
         <label for="Message">Please Enter Your Message</label>
-        <input class="form-control my-size" type="text" name="Message" v-model="form.message" required/>
+        <input class="form-control my-size" type="text" name="message" v-model="form.message" required/>
         <label for="PhoneNumber">Please Enter Your Phone Number (Optional)</label>
-        <input class="form-control my-size" type="tel" name="PhoneNumber" v-model="form.phoneNumber" />
+        <input class="form-control my-size" type="tel" name="phoneNumber" v-model="form.phoneNumber" />
         <input type="submit" class="form-control" />
     </form>
   </div>
